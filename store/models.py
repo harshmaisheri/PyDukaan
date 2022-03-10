6ca534bac59260465e170f2080a29d5865d79a0f
Harsh Maisheri <harshmaisheri@gmail.com>
2022-03-10 07:29:38 +0530

    Migrations;

--- store/models.py
+++ store/models.py
@@ -14,8 +14,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -66,6 +67,7 @@ class Address(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     street = models.CharField(max_length=255),
     city = models.CharField(max_length=255)
+    zip = models.IntegerField(null=True)
 
 
 class Cart (models.Model):
