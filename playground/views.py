2c33a8446d57e9b7a98249594d6b171394bb363a
Harsh Maisheri <harshmaisheri@gmail.com>
2022-03-13 08:14:59 +0530

    Completed ORMs Section

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    return render(request, 'hello.html')
+    # queryset = Product.objects.filter(
+    #     id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+
+    # queryset = Product.objects.prefetch_related(
+    #     'promotions').select_related('collection').all()
+
+    # queryset = Order.objects.select_related(
+    #     'customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+
+    TaggedItem.objects.get_tags_for(Product, 1)
+
+    return render(request, 'hello.html', {'orders': list(queryset)})
