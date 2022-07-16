dd23d04f80c537ec67d52a02b2c4376c420d73a1
Harsh Maisheri <harshmaisheri@gmail.com>
2022-07-16 06:05:53 +0530

    Fix: PyChache

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,12 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
+    try:
+        # send_mail('Testing My 1st Email', 'Hello Whatsup!!',
+        #           'support@hersheys.in', ['info@hersheys.in'])
+        mail_admins('Admin People WhatsUp?', 'message')
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Hershy'})
